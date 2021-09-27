f8616665bc9517780cc63853b5ea3441dfca9a11
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-09-27 15:48:10 +0000

    corrected logic for handling negative value and formula

--- script.js
+++ script.js
@@ -4,31 +4,35 @@ var currentPrice = document.querySelector("#current-price");
 var submitBtn = document.querySelector("#submit-btn");
 var outputBox = document.querySelector("#output-box");
 
+function submitHandler(){
+  var ip = Number(initialPrice.value);
+  var qty = Number(stockQuantity.value);
+  var curr = Number(currentPrice.value);
+if (!ip || !qty || !curr){
+  outputBox.innerText = "Enter al the inputs"
+}
+else if(ip<1 || qty<1 || curr<1){
+  outputBox.innerText = "Enter the positive value greater than zero"
+}
+else{
+calculateProfitAndLoss(ip, qty, curr);
+}
+}
+
 submitBtn.addEventListener("click", submitHandler);
 
 
-function submitHandler(){
-  var ip = initialPrice.value;
-  var qty = stockQuantity.value;
-  var curr = currentPrice.value;
-
-calculateProfitAndLoss(ip, qty, curr);
-
-}
-
 function calculateProfitAndLoss(initial, quantity, current){
-  if(initial<=0 || quantity<=0 || current<=0){
-    return outputBox.innerText = "Enter the positive numbers";
-  }
-  if(initial > current){
-     var loss = (initial - current) * quantity;
-     var lossPercentage = (loss/initial)*100;
-     showOutput(`hey the loss is ${loss} and percent is ${lossPercentage}%`);
-  }else if(current > initial){
-    var profit = (current -initial)*quantity;
-    var profitPercentage = (profit/initial)*100;
+ if(initial < current){
+    var profit = ((current -initial)*quantity).toFixed(2);
+    var profitPercentage = ((profit / initial)*100).toFixed(2);
 
     showOutput(`hey the profit is ${profit} and percent is ${profitPercentage}%`)
+  }
+  else if(initial > current){
+     var loss = ((initial - current) * quantity).toFixed(2);
+     var lossPercentage = ((loss / initial)*100).toFixed(2);
+     showOutput(`hey the loss is ${loss} and percent is ${lossPercentage}%`);
   }else{
     showOutput('kuch nhi mila')
   }

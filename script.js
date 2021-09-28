c825c6bf39fdfe74a5ba89feef5bbfb8d398c765
Mohasin Haque <mohasinhaque23121@gmail.com>
2021-09-28 13:56:41 +0000

    corrected the formula for percentage

--- script.js
+++ script.js
@@ -9,10 +9,10 @@ function submitHandler(){
   var qty = Number(stockQuantity.value);
   var curr = Number(currentPrice.value);
 if (!ip || !qty || !curr){
-  outputBox.innerText = "Enter al the inputs"
+  outputBox.innerText = "Enter all the inputs";
 }
 else if(ip<1 || qty<1 || curr<1){
-  outputBox.innerText = "Enter the positive value greater than zero"
+  outputBox.innerText = "Enter the positive value greater than zero";
 }
 else{
 calculateProfitAndLoss(ip, qty, curr);
@@ -25,13 +25,15 @@ submitBtn.addEventListener("click", submitHandler);
 function calculateProfitAndLoss(initial, quantity, current){
  if(initial < current){
     var profit = ((current -initial)*quantity).toFixed(2);
-    var profitPercentage = ((profit / initial)*100).toFixed(2);
+    var profitPercentage = ((profit) / (initial*quantity))*100;
+     profitPercentage = profitPercentage.toFixed(2);
 
     showOutput(`hey the profit is ${profit} and percent is ${profitPercentage}%`)
   }
   else if(initial > current){
-     var loss = ((initial - current) * quantity).toFixed(2);
-     var lossPercentage = ((loss / initial)*100).toFixed(2);
+     var loss = ((initial - current)*quantity).toFixed(2);
+     var lossPercentage = ((loss) / (initial*quantity))*100;
+     lossPercentage = lossPercentage.toFixed(2);
      showOutput(`hey the loss is ${loss} and percent is ${lossPercentage}%`);
   }else{
     showOutput('kuch nhi mila')
